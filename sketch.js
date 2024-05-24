2834629d4a5f53b01d8ce3cd95e142fd1c28a6b9
rafa0478 <00001143532053SP@al.educacao.sp.gov.br>
2024-05-24 15:06:19 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(500, 400);
+  background("purple");
+}
+
+function draw() {
+  
+  fill("red");
+  stroke("blue");
+  if(mouseIsPressed){}
+rect(mouseX,mouseY,50,10);//retangulo(pos x,pos y,tam x,tam y)
+  
+  
+}
